4cec66079a494e1ba5c61080f83737be5cee0b9b
Sebastien Kempf <s.kempf@live.com>
2020-04-22 10:30:43 +0200

    completed lesson 8

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { createStage, checkCollision } from "../gameHelpers";
+
 //  Styled Components
 import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
 
@@ -16,13 +18,61 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
-  const [stage, setStage] = useStage(player);
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player, resetPlayer);
 
   console.log("re-render");
 
+  const movePlayerX = (dir) => {
+    if (!checkCollision(player, stage, { x: dir, y: 0 }))
+      updatePlayerPos({ x: dir, y: 0 });
+  };
+
+  const startGame = () => {
+    // Reset everything
+    setStage(createStage());
+    resetPlayer();
+    setGameOver(false);
+  };
+
+  const drop = () => {
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      // Game over
+      if (player.pos.y < 1) {
+        console.log("GAME OVER!!!");
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
+  };
+
+  const dropPlayer = () => {
+    drop();
+  };
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      switch (keyCode) {
+        case 37:
+          movePlayerX(-1);
+          break;
+        case 39:
+          movePlayerX(1);
+          break;
+        case 40:
+          dropPlayer();
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
   return (
-    <StyledTetrisWrapper>
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={(e) => move(e)}>
       <StyledTetris>
         <Stage stage={stage} />
         <aside>
@@ -35,7 +85,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
